4c2c720263e5de768e694242320c47880057ffcf
Hrishikesh Shekhar <hrishikeshshekhar@gmail.com>
2020-04-26 00:58:19 +0530

    Add code to analyse IMBD reviews

--- data/imbd_data.py
+++ data/imbd_data.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import string
 
 # Reading all the training_data
@@ -25,7 +26,9 @@ training_data_Y = []
 testing_data_X = []
 testing_data_Y = []
 
-num_files = 500
+num_files = 10000
+lengths = []
+avg_length = 0
 
 # Reading the training data
 
@@ -34,6 +37,7 @@ for filename in train_pos_files[:num_files]:
     filepath = current_dir + training_data_path + pos_dir + filename
     with open(filepath, 'r') as train_file:
         data = train_file.read()
+        lengths.append(len(data.split(' ')))
         train_data[data] = True
 
 # Reading the negative sentences
@@ -41,6 +45,7 @@ for filename in train_neg_files[:num_files]:
     filepath = current_dir + training_data_path + neg_dir + filename
     with open(filepath, 'r') as train_file:
         data = train_file.read()
+        lengths.append(len(data.split(' ')))
         train_data[data] = False
 
 # Reading the testing data
@@ -50,6 +55,7 @@ for filename in test_pos_files[:num_files]:
     filepath = current_dir + testing_data_path + pos_dir + filename
     with open(filepath, 'r') as test_file:
         data = test_file.read()
+        lengths.append(len(data.split(' ')))
         test_data[data] = True
 
 # Reading negative sentences
@@ -57,4 +63,21 @@ for filename in test_neg_files[:num_files]:
     filepath = current_dir + testing_data_path + neg_dir + filename
     with open(filepath, 'r') as test_file:
         data = test_file.read()
+        lengths.append(len(data.split(' ')))
         test_data[data] = False
+
+
+# Evaluating a few metrics on the data
+lengths = np.array(lengths)
+avg_length = np.mean(lengths)
+variance = np.mean((lengths - avg_length) ** 2)
+print("The average length of a sentence in the dataset is : {}").format(avg_length)
+print("The variance of the sentence length is : {}").format(variance)
+
+max_length = np.max(lengths)
+counts = [0 for _ in range(max_length + 1)]
+for length in lengths:
+    counts[length] += 1
+
+plt.plot(counts)
+plt.show()
